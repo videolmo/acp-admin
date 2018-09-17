377d263a211652a4104dd1f6eba23bb98c9bf342
Thibaud Guillaume-Gentil <thibaud@thibaud.gg>
2018-09-17 21:08:45 +0200

    WIP

--- app/assets/javascripts/members.js
+++ app/assets/javascripts/members.js
@@ -16,35 +16,12 @@ var selectDate = function selectDate(dateText) {
   }
 };
 
-var datepickerFallback = function datepickerFallback() {
-  this.dateFields = $('input.date-input');
-  if (this.dateFields.length === 0 || this.dateFields[0].type === 'date') {
-    return;
-  }
-  this.dateFields.datepicker({
-    dateFormat: 'yy-mm-dd',
-    minDate: this.dateFields.attr('min'),
-    maxDate: this.dateFields.attr('max')
-  });
-};
-
-var setDatepickerLocale = function setDatepickerLocale() {
-  var locale = $('body').data('locale');
-  if (locale === 'fr') {
-    $.datepicker.setDefaults($.datepicker.regional['fr-CH']);
-  } else {
-    $.datepicker.setDefaults($.datepicker.regional[locale]);
-  }
-};
-
 var setupDatepicker = function setupDatepicker() {
-  setDatepickerLocale();
   var dateTexts = $('#datepicker').data('dates');
   if (dateTexts) {
     var minDateText = dateTexts[0],
       maxDateText = dateTexts[dateTexts.length - 1];
-    var selectedDateText =
-      $('#datepicker').data('selected-date') || minDateText;
+    var selectedDateText = $('#datepicker').data('selected-date') || minDateText;
     selectDate(selectedDateText);
 
     $('#datepicker').datepicker({
@@ -53,17 +30,17 @@ var setupDatepicker = function setupDatepicker() {
       minDate: minDateText,
       maxDate: maxDateText,
       defaultDate: selectedDateText,
-      onSelect: function onSelect(dateText, inst) {
-        selectDate(dateText);
-      },
-      beforeShowDay: function beforeShowDay(date) {
-        var dateText = $.datepicker.formatDate('yy-mm-dd', date);
-        if (dateTexts.includes(dateText)) {
-          return [true, 'available', null];
-        } else {
-          return [false, null, null];
-        }
-      },
+      // onSelect: function onSelect(dateText, inst) {
+      //   selectDate(dateText);
+      // },
+      // beforeShowDay: function beforeShowDay(date) {
+      //   var dateText = $.datepicker.formatDate('yy-mm-dd', date);
+      //   if (dateTexts.includes(dateText)) {
+      //     return [true, 'available', null];
+      //   } else {
+      //     return [false, null, null];
+      //   }
+      // },
       onChangeMonthYear: function onChangeMonthYear(year, month) {
         var dateText = dateTexts.filter(function(d) {
           var dd = new Date(d);
@@ -86,6 +63,5 @@ var setupDatepicker = function setupDatepicker() {
 $(function() {
   document.addEventListener('turbolinks:load', function() {
     setupDatepicker();
-    datepickerFallback();
   });
 });
